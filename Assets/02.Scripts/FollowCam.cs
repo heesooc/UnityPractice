76363239770fc3a70a450d468fb7a7547f22210f
조희수 <hs.jo.sw.p@gmail.com>
2024-04-30 11:50:11 +0900

    Follow Camera 로직

---
+++ Assets/02.Scripts/FollowCam.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    public Transform targetTr;
+    private Transform camTr;
+
+    [Range(2.0f, 20.0f)]    // 선언할 변수 입력 범위 제한, 인스펙터 뷰 슬라이드바 표시
+    public float distance = 10.0f; // 거리
+
+    [Range(0.0f, 10.0f)]
+    public float height = 2.0f; // 높이
+
+    public float damping = 10.0f; // 반응속도
+
+    private void Start()
+    {
+        camTr = GetComponent<Transform>();
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 pos = targetTr.position + (-targetTr.forward * distance) + (Vector3.up * height);
+        camTr.position = Vector3.Slerp(camTr.position, pos, Time.deltaTime * damping); // Slerp: 주로 회전 로직에 부드럽게 위치 변경
+        camTr.LookAt(targetTr.position); // 카메라 각도 회전
+    }
+
+}
